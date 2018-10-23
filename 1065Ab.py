e2e7d6f4356148fa2489ed23a928063abfccc403
mohit426 <39990195+mohit426@users.noreply.github.com>
2018-10-23 23:41:14 +0530

    update

--- 1065Ab.py
+++ 1065Ab.py
@@ -1,11 +1,12 @@
 http://codeforces.com/contest/1065/problem/A
 
 t=int(input())
-while(t--):
+while(t!=0):
 	s,a,b,c=map(int,input().split())
 	ru=a*c
 	p=(s//ru)*a
 	res=(s//ru)*b
 	res+=p
 	res+=((s%ru)/c)
+	t=t-1
 	print(res)
